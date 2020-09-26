e8e418d604df419d1c5bf8bf329f21c214b0dd25
Mcgowan <Connor.Mcgowan@dcsg.com>
2020-09-26 18:20:11 -0400

    blank creds

--- Get_Top_Songs.py
+++ Get_Top_Songs.py
@@ -5,8 +5,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 
 #setting the client ID and secret to app assignment from API
-cid ="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" #!GET 
-secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" #!GET
+cid ="" #!GET 
+secret = "" #!GET
 
 #Set env variables for client ID, clinet secret, and redirect URL
 os.environ['SPOTIPY_CLIENT_ID']= cid
@@ -15,21 +15,21 @@ os.environ['SPOTIPY_REDIRECT_URI']= 'http:/localhost:8888/callback'
 
 #Auth flow via spotify documentation. User provides login info on first runtime 
 def get_user_creds():
-    username= "" #!GET
+    username= "" 
     client_credentials_manager= SpotifyClientCredentials(client_id=cid, client_secret=secret)
     sp= spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     scope = 'user-top-read'
-    token= util.prompt_for_user_token(username, scope)
+    token= util.prompt_for_user_token(username, scope, cid, secret)
 
     if token:
         sp = spotipy.Spotify(auth=token)
         return("Thanks for logging in", username)
-        return token
+        return token, username
     else:
         return("Can't get token for", username)
 
 #Query last 6 months of users song choices and return top 50
-def get_six_months(token, username):
+#def get_six_months(token, username):
     if token:
         sp = spotipy.Spotify(auth=token)
         topFifty = sp.current_user_top_tracks(limit=50,offset=0,time_range='medium_term')
@@ -41,3 +41,10 @@ def get_six_months(token, username):
                 return("Downloaded top 50 listens for", username)
         else:
             return("Can't get token for", username)
+
+def main():
+    get_user_creds()
+#    get_six_months(token, username)
+
+if __name__ == "__main__":
+    main()
